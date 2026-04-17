7560470ce9a834bb14ba00536c749f0cb54d212d
55Curry <2679019619@qq.com>
2026-04-17 20:22:28 +0800

    Now it's complex

--- exercises/06_stack_maze/06_stack_maze.c
+++ exercises/06_stack_maze/06_stack_maze.c
@@ -1,20 +1,71 @@
+#include <stdbool.h>
 #include <stdio.h>
-
 #define MAX_ROW 5
 #define MAX_COL 5
 
+typedef struct {
+    int r, c;
+} Point;
+
 int maze[MAX_ROW][MAX_COL] = {
-	0, 1, 0, 0, 0,
-	0, 1, 0, 1, 0,
-	0, 0, 0, 0, 0,
-	0, 1, 1, 1, 0,
-	0, 0, 0, 1, 0,
+    0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0,
 };
 
-int main(void)
-{
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+Point stack[MAX_COL * MAX_ROW];
+int top = -1;
 
-	return 0;
+Point predecessor[MAX_ROW][MAX_COL];
+
+void push(Point p) { stack[top++] = p; }
+
+Point pop() { return stack[top--]; }
+
+bool is_empty() { return top == -1; }
+
+void solve() {
+    for (int i = 0; i < MAX_ROW; i++) {
+        for (size_t j = 0; j < MAX_COL; j++) {
+            /* code */
+            predecessor[i][j] = (Point){-1, -1};
+        }
+    }
+
+    Point start = {0, 0};
+    Point end = {4, 4};
+
+    push(start);
+    maze[0][0] = 2;
+
+    while (!is_empty()) {
+        Point curr = pop();
+
+        if (end.c == curr.c && end.r == curr.r) {
+            Point p = end;
+            while (p.r != -1) {
+                printf("(%d, %d)", p.r, p.c);
+                p = predecessor[p.r][p.c];
+            }
+            return;
+        }
+
+        int dr[] = {1, 0, -1, 0};
+        int dc[] = {0, 1, 0, -1};
+
+        for (int i = 0; i < 4; i++) {
+            int nr = dr[i] + curr.r;
+            int nc = dc[i] + curr.c;
+
+            if (nr >= 0 && nr < MAX_ROW && nc >= 0 && nc < MAX_COL) {
+                /* code */
+                maze[nr][nc] = 2;
+                push((Point){nr, nc});
+                predecessor[nr][nc] = curr;
+            }
+        }
+    }
+    printf("No path!\n");
+}
+int main(void) {
+    solve();
+    return 0;
 }
